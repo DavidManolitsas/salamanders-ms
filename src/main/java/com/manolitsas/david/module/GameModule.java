089c57d5000572b0ea641b110dfc37f52f73e6be
David Manolitsas <davidmanolitsas@icloud.com>
2022-03-25 11:49:40 +1100

    added upcoming games endpoint

--- src/main/java/com/manolitsas/david/module/GameModule.java
+++ src/main/java/com/manolitsas/david/module/GameModule.java
@@ -28,6 +28,7 @@ public class GameModule {
     return mapper.toGame(gameResponse);
   }
 
+
   public List<Game> getRecentReleases() {
     List<GameResponse> response = openCriticClient.findRecentReleases();
 
@@ -36,8 +37,18 @@ public class GameModule {
           "No recent games found", "500", HttpStatus.INTERNAL_SERVER_ERROR, "");
     }
 
-    return openCriticClient.findRecentReleases().stream()
-        .map(mapper::toGame)
-        .collect(Collectors.toList());
+    return response.stream().map(mapper::toGame).collect(Collectors.toList());
+  }
+
+
+  public List<Game> getUpcomingReleases() {
+    List<GameResponse> response = openCriticClient.findUpcomingReleases();
+
+    if (response == null) {
+      throw new CustomApiException(
+              "No upcoming games found", "500", HttpStatus.INTERNAL_SERVER_ERROR, "");
+    }
+
+    return response.stream().map(mapper::toGame).collect(Collectors.toList());
   }
 }
